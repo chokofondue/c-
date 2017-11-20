add47cbefaf2f3d10ff0189b78df281fa2bee248
chokofondue <power942@naver.com>
2017-11-20 16:21:33 +0900

    Preprocessor_Directive 모듈의 초기 체크 인

---
+++ Preprocessor_Directive/Program.cs
@@ -0,0 +1,39 @@
+﻿#define TEST_ENV
+//#define PROD_ENV
+
+using System;
+
+namespace Preprocessor_Directive
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            bool verbose = false;
+            //...
+#if(TEST_ENV)
+            Console.WriteLine("Test Environment: Verbose option is set.");
+            verbose = true;
+#else
+            Console.WriteLine("production");
+#endif
+            if (verbose)
+            {
+                //...
+            }
+        }
+
+        #region Public Methods
+        public void Run() { }
+        public void Create() { }
+        #endregion
+
+        #region Public Properties
+        public int Id { get; set; }
+        #endregion
+
+        #region Privates
+        private void Execute() { }
+        #endregion
+    }
+}
