4eeec8971cd54fc70b54b4fcd8834d245076687d
chokofondue <power942@naver.com>
2017-11-21 10:23:39 +0900

    Abstract_Class

---
+++ Abstract_Class/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Abstract_Class
+{
+    public abstract class PureBase{
+        
+        public abstract int GetFirst();
+        public abstract int GetNext();
+    }
+
+    public class DeriverA : PureBase {
+        private int no = 1;
+    }
+
+
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
